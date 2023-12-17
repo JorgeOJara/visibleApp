baabc78585285349b0494a50ede63cd4c5234138
Erick Herrera <erickmherrera01@gmail.com>
2023-12-16 18:23:35 -0700

    modified:   visibleApp/App.js

--- visibleApp/App.js
+++ visibleApp/App.js
@@ -14,21 +14,58 @@ import backgroundImage from "./annie-spratt-wuc-KEIBrdE-unsplash.png";
 
 // Welcome Screen
 function WelcomeScreen({ navigation }) {
+  const [isLoginPressed, setLoginPressed] = useState(false);
+  const [isSignUpPressed, setSignUpPressed] = useState(false);
+
+  const loginPressIn = () => {
+    setLoginPressed(true);
+  };
+
+  const loginPressOut = () => {
+    setLoginPressed(false);
+  };
+
+  const signupPressIn = () => {
+    setSignUpPressed(true);
+  };
+
+  const signupPressOut = () => {
+    setSignUpPressed(false);
+  };
+
+  const loginButtonStyle = {
+    ...styles.welcomeButtonContainer,
+    backgroundColor: isLoginPressed
+      ? "rgba(67,70,75, 0.95)"
+      : "rgba(0, 0, 0, 0.90)",
+  };
+
+  const signUpButtonStyle = {
+    ...styles.welcomeButtonContainer,
+    backgroundColor: isSignUpPressed
+      ? "rgba(67,70,75, 0.95)"
+      : "rgba(0, 0, 0, 0.90)",
+  };
+
   return (
     <View style={styles.container}>
       <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
         <View style={styles.buttonContainer}>
           <Pressable
-            style={styles.welcomeButtonContainer}
+            style={loginButtonStyle}
             onPress={() => navigation.navigate("Login")}
+            onPressIn={loginPressIn}
+            onPressOut={loginPressOut}
           >
-            <Text style={styles.text}>{"Log In"}</Text>
+            <Text style={styles.textWelcome}>{"Log In"}</Text>
           </Pressable>
           <Pressable
-            style={styles.welcomeButtonContainer}
+            style={signUpButtonStyle}
             onPress={() => navigation.navigate("SignUp")}
+            onPressIn={signupPressIn}
+            onPressOut={signupPressOut}
           >
-            <Text style={styles.text}>{"Sign Up"}</Text>
+            <Text style={styles.textWelcome}>{"Sign Up"}</Text>
           </Pressable>
         </View>
       </ImageBackground>
@@ -57,13 +94,10 @@ function LoginScreen() {
     try {
       // Perform signup logic with form data (e.g., make an API call)
       setIsLoading(true);
-      const obj = {email, password };
+      const obj = { email, password };
       //var postData = await postData(url, obj);
-      
       console.log("Logging in:", obj);
       // Navigate to another screen after successful signup
-
-
     } catch (error) {
       console.error("Error logging in:", error.message);
       setError("Login failed. Please try again.");
@@ -71,11 +105,11 @@ function LoginScreen() {
       setIsLoading(false);
     }
   };
-  
+
   return (
     <View style={styles.container}>
       <View style={styles.formContainer}>
-        <Text style={styles.text}>Email Address</Text>
+        <Text style={styles.textLogin}>Email Address</Text>
         <TextInput
           style={styles.textInput}
           placeholder="Email"
@@ -83,7 +117,7 @@ function LoginScreen() {
           onChangeText={(text) => setEmail(text)}
           keyboardType="email-address"
         />
-        <Text style={styles.text}>Password</Text>
+        <Text style={styles.textLogin}>Password</Text>
         <TextInput
           style={styles.textInput}
           placeholder="Password"
@@ -99,7 +133,6 @@ function LoginScreen() {
             disabled={isLoading}
           />
         </View>
-        
       </View>
     </View>
   );
@@ -118,13 +151,11 @@ function SignUpScreen({ navigation }) {
       // Perform signup logic with form data (e.g., make an API call)
       setIsLoading(true);
       const obj = { firstName, lastName, email, password };
-      
+
       //REMOVE COMMENT!!-> var postData = await postData(url, obj);
 
       console.log("Signing up:", obj);
       // Navigate to another screen after successful signup
-
-
     } catch (error) {
       console.error("Error signing up:", error.message);
       setError("Sign up failed. Please try again.");
@@ -187,14 +218,14 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen 
-            name="Welcome" 
-            component={WelcomeScreen} 
-            options={{
-              title: 'Visible App', // Set the title of the header
-              headerStyle: styles.headerStyle,
-              headerTintColor: 'white',
-              headerTitleStyle: styles.headerTitleStyle,
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            title: "Visible App", // Set the title of the header
+            headerStyle: styles.headerStyle,
+            headerTintColor: "white",
+            headerTitleStyle: styles.headerTitleStyle,
           }}
         />
         <Stack.Screen name="Login" component={LoginScreen} />
@@ -220,19 +251,19 @@ const styles = StyleSheet.create({
     paddingLeft: "20%",
   },
   headerStyle: {
-    backgroundColor: 'black',
-    shadowColor: '#000',
-    borderBottomColor: '#2c3e50',
+    backgroundColor: "black",
+    shadowColor: "#000",
+    borderBottomColor: "#2c3e50",
     borderBottomWidth: 0.5,
     height: 90,
   },
 
   headerTitleStyle: {
     fontSize: 25, // Set the font size of the header text
-    fontWeight: 'bold', // You can also set the font weight if needed
-    textShadowColor: 'rgba(240, 243, 244, 0.13)', // Shadow color
+    fontWeight: "bold", // You can also set the font weight if needed
+    textShadowColor: "rgba(240, 243, 244, 0.13)", // Shadow color
     textShadowOffset: { width: 2, height: 2 }, // Shadow offset
-      textShadowRadius: 5, // Shadow radius
+    textShadowRadius: 5, // Shadow radius
   },
 
   backgroundImage: {
@@ -258,13 +289,25 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  text: {
-    fontSize: 16,
-    lineHeight: 21,
+  textWelcome: {
+    fontSize: 20,
+    lineHeight: 22,
     fontWeight: "bold",
-    letterSpacing: 0.25,
+    letterSpacing: 0.5,
     color: "white",
   },
+  textLogin: {
+    fontSize: 20,
+    lineHeight: 22,
+    fontWeight: "bold",
+    letterSpacing: 0.5,
+    color: "white",
+    paddingBottom: 5,
+    textShadowColor: "rgba(240, 243, 244, 0.13)", // Shadow color
+    textShadowOffset: { width: 2, height: 2 }, // Shadow offset
+    textShadowRadius: 5, // Shadow radius
+  },
+
   textInput: {
     fontSize: 12,
     lineHeight: 15,
@@ -289,11 +332,9 @@ const styles = StyleSheet.create({
     backgroundColor: "black",
     width: "70%",
     shadowColor: "white",
-    shadowRadius: 2,
+    shadowRadius: 3,
+    shadowOffset: { width: 4, height: 2 },
   },
-
-  
-  
 });
 
 export default App;
